e250a8f02989537369f3478e93756862482b23a2
Дмитрий Леганьков <you@example.com legankov95@gmail.comgit config --global user.name Asindetongit commit -m 'test cv'$ git config --global user.email you@example.com>
2020-02-19 21:58:15 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined){return 0} else if(array.length === 0){return 0}
+  return array ? Math.min.apply(null,array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined){return 0} else if(array.length === 0){return 0}
+  return array ? Math.max.apply(null,array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined){return 0} else if(array.length === 0){return 0}
+  return  array ? array.reduce((a, b) => (a + b)) / array.length : 0;
 }
